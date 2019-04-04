4031fca849049a2c94bffce900ee6a77b0f10160
Niels Lohmann <mail@nlohmann.me>
2019-04-04 18:12:04 +0200

    :bug: fix regression of #1192

--- include/nlohmann/detail/input/binary_reader.hpp
+++ include/nlohmann/detail/input/binary_reader.hpp
@@ -269,7 +269,7 @@ class binary_reader
             default: // anything else not supported (yet)
             {
                 std::array<char, 3> cr{{}};
-                (std::snprintf)(cr.data(), cr.size(), "%.2hhX", static_cast<unsigned char>(element_type));
+                (std::snprintf)(cr.data(), cr.size(), "%.2X", static_cast<unsigned char>(element_type));
                 return sax->parse_error(element_type_parse_position, std::string(cr.data()), parse_error::create(114, element_type_parse_position, "Unsupported BSON record type 0x" + std::string(cr.data())));
             }
         }
@@ -1925,7 +1925,7 @@ class binary_reader
     std::string get_token_string() const
     {
         std::array<char, 3> cr{{}};
-        (std::snprintf)(cr.data(), cr.size(), "%.2hhX", static_cast<unsigned char>(current));
+        (std::snprintf)(cr.data(), cr.size(), "%.2X", static_cast<unsigned char>(current));
         return std::string{cr.data()};
     }
 
